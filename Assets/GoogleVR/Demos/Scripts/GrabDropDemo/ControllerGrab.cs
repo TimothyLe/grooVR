fb5ec987eb3827a1ee24cb81c544da13e09681d7
Andrew Belamide <andrew.belamide@sjsu.edu>
2018-11-05 00:29:11 -0800

    Grab Drop Demo
    Folder name change

---
+++ Assets/GoogleVR/Demos/Scripts/GrabDropDemo/ControllerGrab.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrab : MonoBehaviour {
+    public bool isContact = false;
+    public bool isGrabbed = false;
+
+    public GameObject objectGrabbed;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Starting.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GvrController.ClickButtonDown)
+        {
+            if(isContact == true)
+            {
+                if (isGrabbed == false)
+                {
+                    Debug.Log("Grab button pressed.");
+                    isGrabbed = true;
+                    objectGrabbed.GetComponent<Transform>().parent = gameObject.transform;
+                    objectGrabbed.GetComponent<Rigidbody>().isKinematic = true;
+                    //pickupcan
+                }
+                else
+                {
+                    Debug.Log("Grab button released.");
+                    isGrabbed = false;
+                    objectGrabbed.GetComponent<Transform>().parent = null;
+                    objectGrabbed.GetComponent<Rigidbody>().isKinematic = false;
+                    //dropobject
+                }
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision enter");
+        isContact = true;
+        objectGrabbed = other.gameObject;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        isContact = false;
+        Debug.Log("Collision Exit.");
+    }
+}
---
+++ Assets/GoogleVR/Demos/Scripts/GrabDropDemo/ResetPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+    private Vector3 startPos;
+    private Quaternion startRot; //handles rotation
+
+	// Use this for initialization
+	void Start () {
+
+        startPos = GetComponent<Transform>().position;
+        startRot = GetComponent<Transform>().rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GvrController.AppButton)
+        {
+            gameObject.GetComponent<Transform>().position = startPos;
+            gameObject.GetComponent<Transform>().rotation = startRot;
+        }
+	
+	}
+}
